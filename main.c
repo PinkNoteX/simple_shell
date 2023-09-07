bc7211437bc71936b1aa2a24f28cd43f1f495a35
PinkNoteX <omarfarghaly8@gmail.com>
2023-09-06 23:14:27 -0700

    Simple foundation

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
-main function
+#include "main.h"
+
+int main(void)
+{
+	char *cmd = ":) ";
+	char *userinput;
+	size_t n = 0;
+	ssize_t nchars;
+	while (1)
+	{
+		printf("%s",cmd);
+		nchars = getline(&userinput, &n, stdin);
+		
+		if (nchars == -1)
+		{
+			printf("\n");
+			free (userinput);
+			return (-1);
+		}
+	printf("%s", userinput);
+	}
+	free(userinput);
+	return (0);
+}
