8cc7af4ce9cc08e298ec08f654c8273613b8cb37
PinkNoteX <omarfarghaly8@gmail.com>
2023-09-06 22:04:27 -0700

    main.h libs

--- main.h
+++ main.h
@@ -1,1 +1,13 @@
-prototypes here and libraries
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+#endif
