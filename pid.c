1937a7fc8190f9e98c14f93e06ad35bb4c15a8a0
PinkNoteX <omarfarghaly8@gmail.com>
2023-09-06 20:54:45 -0700

    PPID exercise

---
+++ pid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t my_pid;
+    pid_t parent_pid;
+
+    my_pid = getpid();
+    while (my_pid != NULL)
+	    my_pid = getpid();
+
+    printf("%u\n", my_pid);
+    parent_pid = getppid();
+    printf("%u\n", parent_pid);
+    return (0);
+}
