c52b28889a179432281303b9c770ade91ba9895e
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-08 17:03:28 -0700

    betty3

--- main.c
+++ main.c
@@ -2,32 +2,32 @@
 
 int main(void)
 {
-        char **av, *userinput, *cmd = ":) ",
+	char **av, *userinput, *cmd = ":) ",
 	     **sarray = malloc(25 * sizeof(char *));
-        int i = 0,  arraysize = 0;
+	int i = 0,  arraysize = 0;
 
-        while (1)
-        {
-        printf("%s", cmd);
-        userinput = userin();
-        tokeniser(userinput, sarray);
-        for (i = 0; sarray[i] != NULL; i++)
-        {
-                arraysize = i + 2; }
-        i = 0;
-        av = malloc(sizeof(char *) * arraysize);
-        while (sarray[i] != NULL)
-        {
-                av[i] = sarray[i];
-                        i++; }
-        av[i] = sarray[i];
-        /*Start Execution*/
-        for (i = 0; i < arraysize - 1; i++)
-        {
-        printf("%s\n", av[i]);
-        }}
-        free_array(sarray, 25);
-        free_array(av, arraysize);
-        free(userinput);
-        return (0);
+	while (1)
+	{
+		printf("%s", cmd);
+		userinput = userin();
+		tokeniser(userinput, sarray);
+		for (i = 0; sarray[i] != NULL; i++)
+		{
+			arraysize = i + 2; }
+		i = 0;
+		av = malloc(sizeof(char *) * arraysize);
+		while (sarray[i] != NULL)
+		{
+			av[i] = sarray[i];
+			i++; }
+		av[i] = sarray[i];
+		/*Start Execution*/
+		for (i = 0; i < arraysize - 1; i++)
+		{
+			printf("%s\n", av[i]);
+		}}
+	free_array(sarray, 25);
+	free_array(av, arraysize);
+	free(userinput);
+	return (0);
 }
--- userin.c
+++ userin.c
@@ -1,15 +1,16 @@
 #include "main.h"
 char *userin(int ret)
 {
-        char *userin_s;
-        size_t n = 0;
-        ssize_t nchars;         
-         nchars = getline(&userin_s, &n, stdin);
-                if (nchars == -1)
-                {
-                        printf("\n");
-                        free(userin_s);
-                        exit (0);
-                }
-            return(userin_s);
+	char *userin_s;
+	size_t n = 0;
+	ssize_t nchars;
+
+	nchars = getline(&userin_s, &n, stdin);
+	if (nchars == -1)
+	{
+		printf("\n");
+		free(userin_s);
+		exit(0);
+	}
+	return (userin_s);
 }
