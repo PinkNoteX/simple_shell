06f3720195490eb80df7ae5f2a507913afced7e7
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-17 16:31:08 -0700

    echocheck.c betty

--- echocheck.c
+++ echocheck.c
@@ -2,35 +2,35 @@
 
 void echocheck(char **av)
 {
-    int i = 1,r = 0;
-    char *cmd;
+	int i = 1, r = 0;
+	char *cmd;
 
-    if(av)
-    {
-        cmd = av[0];
-    }
-
-if(strcmp(cmd, "echo") == 0 ||
- strcmp(cmd, "/bin/echo") == 0 ||
- strcmp(cmd, "/usr/bin/echo") == 0)
+	if (av)
 {
-
-while (av[i] != NULL)
-{
-    while (av[i][r] != '\0')
-    {
-    if(av[i][r] == '"' || av[i][r] == '\'')
-    {
-       removeChar(av[i], '"', '\'');
-    }
-     r++;
-    }
-    i++;
+	cmd = av[0];
 }
-i = 0;
-if (av[i] != NULL && av[i+2] != NULL)
+
+	if (strcmp(cmd, "echo") == 0 ||
+	strcmp(cmd, "/bin/echo") == 0 ||
+	strcmp(cmd, "/usr/bin/echo") == 0)
 {
-    removefirstspace(av[2]);
+
+	while (av[i] != NULL)
+{
+	while (av[i][r] != '\0')
+{
+	if (av[i][r] == '"' || av[i][r] == '\'')
+{
+	removeChar(av[i], '"', '\'');
+}
+	r++;
+}
+	i++;
+}
+	i = 0;
+	if (av[i] != NULL && av[i + 2] != NULL)
+{
+	removefirstspace(av[2]);
 }
 }
 }
