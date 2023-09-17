c04c00080bab067cc442f57820555b9d81ba5560
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-17 15:52:09 -0700

    chcmd.c

--- chcmd.c
+++ chcmd.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
- void chcmd(char **av, char *actcmdd,char **env,char *enterpath, int counter){
-    char * command;
-    unsigned int i;
+void chcmd(char **av, char *actcmdd, char **env, char *enterpath, int counter)
+{
+	char *command;
+	unsigned int i;
 
-    command = av[0];
-    {
-    if(strcmp(command, "env") == 0 || strcmp(command, "printenv") == 0)
-    {
-    i = 0;
-    while (env[i] != NULL)
-    {
-        write(STDOUT_FILENO, env[i], strlen(env[i]));
-        write(STDOUT_FILENO,"\n",1);
-        i++;
-    }
-    exit (0);
-    }
-    else if (strcmp(command, " ") == 0)
-    {
-        exit (errno);
-    }
-    else
-    {
-        cmdexc(av,actcmdd,env,enterpath,counter);
-    }}
- }
+	command = av[0];
+{
+	if (strcmp(command, "env") == 0 || strcmp(command, "printenv") == 0)
+{
+	i = 0;
+	while (env[i] != NULL)
+{
+	write(STDOUT_FILENO, env[i], strlen(env[i]));
+	write(STDOUT_FILENO, "\n", 1);
+	i++;
+}
+	exit(0);
+}
+	else if (strcmp(command, " ") == 0)
+{
+	exit(errno);
+}
+	else
+{
+	cmdexc(av, actcmdd, env, enterpath, counter);
+}}
+}
--- cmdchecker.c
+++ cmdchecker.c
@@ -1,30 +1,29 @@
 #include "main.h"
 char *cmdchecker(char **array, char *cmd)
 {
-        int i;
-        struct stat buf;
-        char cmdarray[10000];
-        char arraystore[10000];
-        char *actualcmd = malloc(10000 * sizeof(char));
-        
-            
-        strcpy(cmdarray, cmd);
-        for( i = 0; array[i]!= NULL; i++)
-        {
-                if(cmdarray[0] != '/')
-                {
-                strcpy(arraystore, array[i]);
-                strcat(arraystore, cmdarray);
-                }
-                else
-                strcpy(arraystore, cmdarray);
-               if(stat(arraystore, &buf) == 0)
-               {
-                strcpy(actualcmd, arraystore);
-                return (actualcmd);
-               }
-        }
-                free(actualcmd);
-                return(cmd);
-        
+	int i;
+	struct stat buf;
+	char cmdarray[10000];
+	char arraystore[10000];
+	char *actualcmd = malloc(10000 * sizeof(char));
+
+
+	strcpy(cmdarray, cmd);
+	for( i = 0; array[i] != NULL; i++)
+{
+		if (cmdarray[0] != '/')
+{
+		strcpy(arraystore, array[i]);
+		strcat(arraystore, cmdarray);
+}
+		else
+		strcpy(arraystore, cmdarray);
+		if (stat(arraystore, &buf) == 0)
+{
+		strcpy(actualcmd, arraystore);
+		return (actualcmd);
+}
+		free(actualcmd);
+		return (cmd);
+
 }
