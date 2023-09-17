cd7897f6df319e3fe96d99d22170758ed5e1dc81
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-17 16:23:55 -0700

    commentchecker.c betty

--- commentchecker.c
+++ commentchecker.c
@@ -1,33 +1,36 @@
 #include "main.h"
-char **commentche(char **av){
+char **commentche(char **av)
+{
 int i = 0, comment_int, r;
 bool comment = false;
 
-while (av[i] != NULL)
+	while (av[i] != NULL)
 {
-    if(av[i][0] == '#')
-    {
-        comment_int = i;
-        comment = true;
-    }
-    i++;
+	if (av[i][0] == '#')
+{
+	comment_int = i;
+	comment = true;
 }
-if(comment == true)
+	i++;
+}
+	if (comment == true)
 {
-    r = comment_int;
-    if(r == 0){
-        strcpy(av[0], " ");
-        r++;
-    }
-    else{
-    while (r < i + 1)
-    {
-        av[r] = '\0';
-        r++;
-    }
-    return (av);}
+	r = comment_int;
+	if (r == 0)
+{
+	strcpy(av[0], " ");
+	r++;
+}
+	else
+{
+	while (r < i + 1)
+{
+	av[r] = '\0';
+	r++;
+}
+	return (av);
+}
 
 }
-return (av);
+	return (av);
 }
-
