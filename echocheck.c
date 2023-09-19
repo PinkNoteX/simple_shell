00e875d7b20f7b103dedf76363b71b77ba397d00
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-19 06:02:08 -0700

    echocheck betty and tweaks

--- echocheck.c
+++ echocheck.c
@@ -1,36 +1,28 @@
 #include "main.h"
-
+/**
+ * echocheck - checks if the command entered is "echo".
+ * @av: args array.
+ * Return: void
+ */
 void echocheck(char **av)
 {
-	int i = 1, r = 0;
-	char *cmd;
+int i = 1, r = 0;
+char *cmd;
 
-	if (av)
+if (av)
+cmd = av[0];
+if (_strcmp(cmd, "echo") == 0 ||
+_strcmp(cmd, "/bin/echo") == 0 ||
+_strcmp(cmd, "/usr/bin/echo") == 0)
 {
-	cmd = av[0];
-}
-
-	if (strcmp(cmd, "echo") == 0 ||
-	strcmp(cmd, "/bin/echo") == 0 ||
-	strcmp(cmd, "/usr/bin/echo") == 0)
+while (av[i] != NULL)
 {
-
-	while (av[i] != NULL)
+while (av[i][r] != '\0')
 {
-	while (av[i][r] != '\0')
-{
-	if (av[i][r] == '"' || av[i][r] == '\'')
-{
-	removeChar(av[i], '"', '\'');
-}
-	r++;
-}
-	i++;
-}
-	i = 0;
-	if (av[i] != NULL && av[i + 2] != NULL)
-{
-	removefirstspace(av[2]);
-}
-}
-}
+if (av[i][r] == '"' || av[i][r] == '\'')
+removeChar(av[i], '"', '\'');
+r++; }
+i++; }
+i = 0;
+if (av[i] != NULL && av[i + 2] != NULL)
+removefirstspace(av[2]); }}
