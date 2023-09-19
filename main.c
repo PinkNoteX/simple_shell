b6667d702cd73587664602d20438b29c05df1fe8
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-19 05:59:08 -0700

    main.c passes betty check and other twaeks

--- main.c
+++ main.c
@@ -1,72 +1,51 @@
 #include "main.h"
 
+/**
+ * main - main function
+ * @ac: number of args.
+ * @av: pointer to pointer to char.
+ * @env: env array.
+ * Return: exitval
+ */
 int main(int ac, char **av, char **env)
 {
-        char *userinput, *Path_string, *cmd = ":) ", *end,
-             **Paths = malloc(30 * sizeof(char *)), *actualcommand,
-             *enterpath;
-        int i = 0,  arraysize = 100, counter = 1, exitvalue = 0,
-        waitstatus = 0;
-        pid_t pid;
-        bool piped = false;
-        (void)ac;
-
-        enterpath = av[0];
-        while (1 && !piped)
-        {
-            if(isatty(STDIN_FILENO) == 0)
-             piped = false;
-             else{
-                write(STDOUT_FILENO, cmd, 3);}
-                userinput = userin();
-        if(strcmp(userinput, "$()^($)!$($)$!") != 0
-        && strcmp(userinput, " ") != 0){
-        av = spilt(userinput," ",&arraysize);
-        if(av[0] != NULL){
-        echocheck(av);
-        Path_string = getenv("PATH");
-       if(Path_string != NULL && Path_string != ""){
-        Path_Tokenizer(Path_string,Paths);}
-        else{
-            Path_string = malloc(15);
-            strcpy(Path_string, "/bin:/usr/bin");
-            Path_Tokenizer(Path_string,Paths);
-            free(Path_string);}
-        av = commentche(av);
-        if(strcmp(av[0], "exit") == 0)
-        {
-            if(av[1] != NULL)
-            {
-            exitvalue = strtol(av[1], &end, 10);;
-
-            if (end == av[1] || *end != '\0' || errno == ERANGE)
-            {
-                perror(":( Error:\n");
-            }
-            else
-            exit (exitvalue);
-            }
-            else
-            exit (waitstatus);
-        }
-        else
-        {
-        actualcommand = cmdchecker(Paths,av[0]);
-        if(actualcommand != NULL)
-        pid = fork();
-        if (pid == 0)
-        {
-        chcmd(av,actualcommand,env,enterpath,counter);
-        }}
-        
-        wait(&waitstatus);
-        waitstatus = WEXITSTATUS(waitstatus);
-        counter++;
-        }}}
-        free_array(Paths, 30);
-        free(userinput);
-        free_array(av, arraysize);
-
-
-        return (0);
-}
+char *in, *P, *cmd = ":) ", **Paths = malloc(30 * sizeof(char *)), *act, *ep;
+int az = 100, counter = 1, exitval = 0, ws = 0, pipefd[2], exitbool = 0;
+pid_t pid;
+(void)ac;
+ep = av[0];
+while (1)
+{
+pipe(pipefd);
+if (isatty(STDIN_FILENO) != 0)
+write(STDOUT_FILENO, cmd, 3);
+in = userin(exitval, in, Paths, av);
+if (_strcmp(in, " ") != 0)
+{
+av = split(in, " ", &az);
+echocheck(av);
+P = _getenv("PATH");
+if (P == NULL)
+{
+P = malloc(18);
+_strcpy(P, "/bi:/ubins");
+Path_Tokenizer(P, Paths);
+free(P); }
+else
+Path_Tokenizer(P, Paths);
+av = commentche(av);
+if (_strcmp(av[0], "exit") == 0)
+exithandler(av, exitval, ep, exitbool, counter, Paths, in);
+else
+{
+act = cmdchecker(Paths, av[0], &exitval);
+if (act != NULL)
+pid = fork();
+if (pid == 0)
+chcmd(av, act, env, ep, counter, pipefd); }
+wait(&ws);
+ws = WEXITSTATUS(ws);
+exitval = piped(pipefd, ws);
+counter++; }}
+free_helper(in, Paths, av);
+return (exitval); }
