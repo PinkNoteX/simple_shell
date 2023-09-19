2149f948f1133967408ab4ec06049f954950e812
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-19 05:54:38 -0700

    betty and some twaeks to chcmd.c

--- chcmd.c
+++ chcmd.c
@@ -1,29 +1,32 @@
 #include "main.h"
+/**
+ * chcmd - checks if command is build in or not.
+ * @av: args array.
+ * @actcmdd: the actual command
+ * @env: the env array.
+ * @enterpath: path program launched through.
+ * @counter: the count of loops that passed
+ * @exitpipe: pipe to pass a value to main process.
+ * Return: void
+ */
+void chcmd(char **av, char *actcmdd, char **env,
+char *enterpath, int counter, int exitpipe[2]){
+char *command;
+int i;
 
-void chcmd(char **av, char *actcmdd, char **env, char *enterpath, int counter)
+command = av[0];
 {
-	char *command;
-	unsigned int i;
-
-	command = av[0];
+if (_strcmp(command, "env") == 0 ||
+_strcmp(command, "printenv") == 0)
 {
-	if (strcmp(command, "env") == 0 || strcmp(command, "printenv") == 0)
+for (i = 0; env[i]; i++)
 {
-	i = 0;
-	while (env[i] != NULL)
-{
-	write(STDOUT_FILENO, env[i], strlen(env[i]));
-	write(STDOUT_FILENO, "\n", 1);
-	i++;
+write(STDOUT_FILENO, env[i], _strlen(env[i]));
+write(STDOUT_FILENO, "\n", 1); }
+exit(0);
 }
-	exit(0);
-}
-	else if (strcmp(command, " ") == 0)
-{
-	exit(errno);
-}
-	else
-{
-	cmdexc(av, actcmdd, env, enterpath, counter);
+else if (_strcmp(command, " ") == 0)
+exit(errno);
+else
+cmdexc(av, actcmdd, env, enterpath, counter, exitpipe);
 }}
-}
