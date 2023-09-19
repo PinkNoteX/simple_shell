1930ac420955f9771fff17b0b35bacb82695f1d4
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-19 06:04:03 -0700

    cmdchecker.c betty and tweaks

--- cmdchecker.c
+++ cmdchecker.c
@@ -1,29 +1,31 @@
 #include "main.h"
-char *cmdchecker(char **array, char *cmd)
+/**
+ * cmdchecker - checks if the command does exist in path.
+ * @array: array of the PATH enviroment tokenized
+ * @cmd: command to check for.
+ * @exitstatus: exit status value.
+ * Return: actualcmd on sucess or old command if failure.
+ */
+char *cmdchecker(char **array, char *cmd, int *exitstatus)
 {
-	int i;
-	struct stat buf;
-	char cmdarray[10000];
-	char arraystore[10000];
-	char *actualcmd = malloc(10000 * sizeof(char));
+int i;
+struct stat buf;
+char cmdarray[10000], arraystore[10000];
+char *actualcmd = malloc(100 * sizeof(char));
 
-
-	strcpy(cmdarray, cmd);
-	for (i = 0; array[i] != NULL; i++)
+_strcpy(cmdarray, cmd);
+for (i = 0; array[i] != NULL; i++)
 {
-		if (cmdarray[0] != '/')
+if (cmdarray[0] != '/')
 {
-		strcpy(arraystore, array[i]);
-		strcat(arraystore, cmdarray);
-}
-		else
-		strcpy(arraystore, cmdarray);
-		if (stat(arraystore, &buf) == 0)
+_strcpy(arraystore, array[i]);
+_strcat(arraystore, cmdarray); }
+else
+_strcpy(arraystore, cmdarray);
+if (stat(arraystore, &buf) == 0)
 {
-		strcpy(actualcmd, arraystore);
-		return (actualcmd);
-}
-		free(actualcmd);
-		return (cmd);
-
-}
+_strcpy(actualcmd, arraystore);
+*exitstatus = 0;
+return (actualcmd); }}
+free(actualcmd);
+return (cmd); }
