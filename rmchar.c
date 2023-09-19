36d6483e471051470e0f4ca490a498d70067cdc1
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-19 05:57:34 -0700

    rmchar betty and tweaks

--- rmchar.c
+++ rmchar.c
@@ -1,19 +1,31 @@
 #include "main.h"
 
+/**
+ * removeChar - removes spesific characters from string.
+ * @str: str to remove characters from.
+ * @garbage1: char to remove.
+ * @garbage2: another char to remove.
+ * Return: void
+ */
 void removeChar(char *str, char garbage1, char garbage2)
 {
-    char *src, *dst;
-    for (src = dst = str; *src != '\0'; src++) {
-        if (*src != garbage1 && *src != garbage2) {
-            *dst = *src;
-            dst++;
-        }
-    }
-    *dst = '\0';
-}
+char *src, *dst;
+for (src = dst = str; *src != '\0'; src++)
+{
+if (*src != garbage1 && *src != garbage2)
+{
+*dst = *src;
+dst++;
+}}
+*dst = '\0'; }
+
+/**
+ * removefirstspace - Removes first space from string
+ * @str: str to remove space from.
+ * Return: void
+ */
 void removefirstspace(char *str)
 {
-    if (str[0] != '\0') {
-        strcpy(str, str + 1);
-    }
-}
+if (str[0] != '\0')
+{
+_strcpy(str, str + 1); }}
