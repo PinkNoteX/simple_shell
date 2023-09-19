5fd14cc6ebf5207ab6ab21342e529ae737f90aec
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-19 06:02:36 -0700

    commentchecker.c

--- commentchecker.c
+++ commentchecker.c
@@ -1,36 +1,33 @@
 #include "main.h"
+/**
+ * commentche - checks if there is a comment char
+ * @av: args array.
+ * Return: av with null after the comment.
+ */
 char **commentche(char **av)
 {
 int i = 0, comment_int, r;
 bool comment = false;
 
-	while (av[i] != NULL)
+while (av[i] != NULL)
 {
-	if (av[i][0] == '#')
+if (av[i][0] == '#')
 {
-	comment_int = i;
-	comment = true;
-}
-	i++;
-}
-	if (comment == true)
+comment_int = i;
+comment = true; }
+i++; }
+if (comment == true)
 {
-	r = comment_int;
-	if (r == 0)
+r = comment_int;
+if (r == 0)
 {
-	strcpy(av[0], " ");
-	r++;
-}
-	else
+_strcpy(av[0], " ");
+r++; }
+else
 {
-	while (r < i + 1)
+while (r < i + 1)
 {
-	av[r] = '\0';
-	r++;
-}
-	return (av);
-}
-
-}
-	return (av);
-}
+av[r] = '\0';
+r++; }
+return (av); }}
+return (av); }
