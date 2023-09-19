a818fb200a358c5533061bf77e2a5f955a0f80bf
eslameladwy <eslam.eladwy9@gmail.com>
2023-09-19 05:56:01 -0700

    userinput betty and other tweaks

--- userin.c
+++ userin.c
@@ -1,42 +1,49 @@
 #include "main.h"
-char *userin()
-{
-        char *userin_s;
-        size_t n = 0;
-        ssize_t nchars;
 
-        nchars = getline(&userin_s, &n, stdin);
-        if (nchars == -1)
-        {
-                  if (feof(stdin))
-        {
-                if(isatty(STDIN_FILENO) == 0){
-                free(userin_s);
-                exit (0);
-                }
-            clearerr(stdin);
-            write(STDOUT_FILENO,"\n",1);
-            exit (0);
-        }
-        else{
-                if(isatty(STDIN_FILENO) == 0){
-                free(userin_s);
-                exit (0);
-                }
-                free(userin_s);
-                write(STDOUT_FILENO,"\n",1);
-                exit (0);
-        }}
-        if ((userin_s)[nchars - 1] == '\n')
-        {
-        (userin_s)[nchars - 1] = '\0';
-        --nchars;
-        }
-        if (nchars == 0)
-        {
-                strcpy(userin_s,"$()^($)!$($)$!");
-                return(userin_s);
-        }
-        else
-        return (userin_s);
-}
+/**
+ * userin - Gets user input.
+ * @exitval: exit value to exit the program with.
+ * @userinput: for free on exit
+ * @Paths: for free on exit
+ * @av: for free on exit
+ * Return: userinput
+ */
+char *userin(int exitval, char *userinput, char **Paths, char **av)
+{
+char *userin_s;
+size_t n = 0;
+ssize_t nchars;
+nchars = getline(&userin_s, &n, stdin);
+if (nchars == -1)
+{
+if (feof(stdin))
+{
+if (isatty(STDIN_FILENO) == 0)
+{
+free(userin_s);
+free_helper(userinput, Paths, av);
+exit(exitval); }
+clearerr(stdin);
+write(STDOUT_FILENO, "\n", 1);
+free_helper(userinput, Paths, av);
+exit(exitval); }
+else
+{
+if (isatty(STDIN_FILENO) == 0)
+{
+free(userin_s);
+exit(exitval); }
+free(userin_s);
+write(STDOUT_FILENO, "\n", 1);
+free_helper(userinput, Paths, av);
+exit(exitval); }}
+if ((userin_s)[nchars - 1] == '\n')
+{
+(userin_s)[nchars - 1] = '\0';
+--nchars; }
+if (nchars == 0)
+{
+_strcpy(userin_s, " ");
+return (userin_s); }
+else
+return (userin_s); }
